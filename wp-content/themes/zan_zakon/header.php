736f29f216758d20aef89d9540d945db0990f148
elessarbaz <darmen89@yandex.ru>
2016-07-22 14:39:57 +0600

    Внесение изменений в вёрстку

--- wp-content/themes/zan_zakon/header.php
+++ wp-content/themes/zan_zakon/header.php
@@ -5,7 +5,6 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php //wp_head() ?>
-	<link href='https://fonts.googleapis.com/css?family=Comfortaa:400,700&subset=latin,cyrillic-ext' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="<?php bloginfo('template_directory')?>/public/css/uikit.min.css">
 	<link rel="stylesheet" href="<?php bloginfo('template_directory')?>/public/css/components/accordion.min.css">
 	<link rel="stylesheet" href="<?php bloginfo('template_directory')?>/public/css/components/sticky.min.css">
