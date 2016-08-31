f4010ac181935222b5823278997af561b7f5d15d
Sergey Demidov <aspirin_1988@mail.ru>
2016-08-31 17:56:23 +0600

    /start

--- wp-content/themes/zan_zakon/template-parts/content-single.php
+++ wp-content/themes/zan_zakon/template-parts/content-single.php
@@ -1,22 +1,26 @@
 <br>
 <br>
 <br>
+<br>
+<br>
 <!--НАЧАЛО publications-->
 <div class="publications" id="publications">
 	<div class="uk-container uk-container-center">
 		<h2><?=get_the_title()?></h2>
 		<?php the_content() ?>
 		<!--НАЧАЛО Unitegallery-->
+		<?php if ($gallery=pp_gallery_get()):?>
 		<div id="unitegallery-1" style="display:none;">
 			<img alt="Image" src="<?=get_the_post_thumbnail_url()?>"
 				 data-image="<?=get_the_post_thumbnail_url()?>"
 				 data-description="<?=get_the_title()?>">
-			<?php foreach (pp_gallery_get() as $image): ?>
+			<?php foreach ($gallery as $image): ?>
 			<img alt="Image" src="<?=$image->url?>"
 				 data-image="<?=$image->url?>"
 				 data-description="<?=$image->alt?>">
 			<?php endforeach; ?>
 		</div>
+		<?php endif; ?>
 		<!--КОНЕЦ Unitegallery-->
 	</div>
 </div>
